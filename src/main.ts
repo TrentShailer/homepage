20fc3dfb86404dde4c3a6eb9c92dc189d732a6fe
Trent Shailer <trent.shailer@gmail.com>
2024-01-10 22:15:15 +1300

    Removed react and changed to pnpm

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import "./style.css";
+import "./card.css";
+import "./footer.css";
+
+const colors = [
+    ["#e53e3e", "#9b2c2c"],
+    ["#dd6b20", "#9c4221"],
+    ["#d69e2e", "#975a16"],
+    ["#38a169", "#276749"],
+    ["#319795", "#285e61"],
+    ["#3182ce", "#2c5282"],
+    ["#00b5d8", "#0987a0"],
+    ["#805ad5", "#553c9a"],
+    ["#d53f8c", "#97266d"],
+];
+
+// find all the card background and assign them their color when they are hovered and unhovered
+const cards = document.querySelectorAll<HTMLElement>(".card");
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    const bg = card.querySelector<HTMLElement>(".card-bg")!;
+    const color_index = Math.floor(Math.random() * colors.length);
+    bg.setAttribute("style", `background: ${colors[color_index][0]}`);
+
+    const icon = card.querySelector<HTMLElement>(".card-icon")!;
+    icon.setAttribute("style", `color:${colors[color_index][0]};opacity:0.20;`);
+
+    card.addEventListener("mouseenter", () => {
+        icon.setAttribute(
+            "style",
+            `color:${colors[color_index][1]};opacity:0.33;`
+        );
+    });
+    card.addEventListener("mouseleave", () => {
+        icon.setAttribute(
+            "style",
+            `color:${colors[color_index][0]};opacity:0.20;`
+        );
+    });
+}
