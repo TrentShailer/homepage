fa40f62a5a78d86690dad447da24a379ad02e297
Trent Shailer <trent.shailer@gmail.com>
2024-01-11 13:40:42 +1300

    improved color randomisation

--- src/main.ts
+++ src/main.ts
@@ -14,28 +14,32 @@ const colors = [
     ["#d53f8c", "#97266d"],
 ];
 
+let remaining_colors = Array.from(colors);
+
 // find all the card background and assign them their color when they are hovered and unhovered
 const cards = document.querySelectorAll<HTMLElement>(".card");
 
 for (let i = 0; i < cards.length; i++) {
+    if (remaining_colors.length === 0) {
+        remaining_colors = Array.from(colors);
+    }
+
+    const color_index = Math.floor(Math.random() * remaining_colors.length);
+    const color = remaining_colors[color_index];
+    remaining_colors.splice(color_index, 1);
+
     const card = cards[i];
     const bg = card.querySelector<HTMLElement>(".card-bg")!;
-    const color_index = Math.floor(Math.random() * colors.length);
-    bg.setAttribute("style", `background: ${colors[color_index][0]}`);
+
+    bg.setAttribute("style", `background: ${color[0]}`);
 
     const icon = card.querySelector<HTMLElement>(".card-icon")!;
-    icon.setAttribute("style", `color:${colors[color_index][0]};opacity:0.20;`);
+    icon.setAttribute("style", `color:${color[0]};opacity:0.20;`);
 
     card.addEventListener("mouseenter", () => {
-        icon.setAttribute(
-            "style",
-            `color:${colors[color_index][1]};opacity:0.33;`
-        );
+        icon.setAttribute("style", `color:${color[1]};opacity:0.33;`);
     });
     card.addEventListener("mouseleave", () => {
-        icon.setAttribute(
-            "style",
-            `color:${colors[color_index][0]};opacity:0.20;`
-        );
+        icon.setAttribute("style", `color:${color[0]};opacity:0.20;`);
     });
 }
