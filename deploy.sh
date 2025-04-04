28f93014f84309252b0628ead4af505b723ad7dd
Trent Shailer <trent.shailer@gmail.com>
2025-04-05 10:53:37 +1300

    Simplify site setup

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-pnpm build
-rm -rf /home/www-data/trentshailer.com/dist
-mkdir -p /home/www-data/trentshailer.com/dist
-cp nginx.conf /home/www-data/trentshailer.com/nginx.conf
-mv dist /home/www-data/trentshailer.com
